a887442e7b68959510075cc3ac5b28c15d7ad9cf
Mulia Arifandi Nasution <mul14.net@gmail.com>
2019-02-17 18:58:15 +0700

    Initial commit

---
+++ precache-manifest.8ad32e669f88f13a2e906d8f02b73e2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "959f75c72e456d942d99",
+    "url": "/css/app.f2fd9bac.css"
+  },
+  {
+    "revision": "959f75c72e456d942d99",
+    "url": "/js/app.914901fb.js"
+  },
+  {
+    "revision": "c258a4f9333e7b4525b6",
+    "url": "/js/chunk-vendors.28582740.js"
+  },
+  {
+    "revision": "13943f68a9724a8a099af3f49c1e8c4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
